bfb63de59f2f07cd7e4a5eef3b6ede17437a6b2d
Anjali Pandurang Gondse <anjaligondse1996@gmail.com>
2022-08-10 21:59:35 +0530

    first commit

---
+++ Teleco minicapstone project.sql
@@ -0,0 +1,18 @@
+SELECT * FROM teleco_company.basic_details;
+
+#This data set contains the data of customers of a fictional telco company that provided home phone and Internet services in California in Quarter 3 of the fiscal year.
+#In this data set there is information about 7043 customers to which this telco company provides home phone and Internet services.
+#This Teleco_company schema has following tables
+#basic_details 
+#Location
+#Customer account details
+#services provided
+#churn_related
+#customerID is a primary key in this database
+#to create this mutliple table we have to follow following steps
+#1. to create new schema teleco_company
+#2. by right clicking on tables choose add table
+#3. We have to give name to the table according to the table contents, after that we have to add columns also we have to assign proper datatypes and other attributes.
+#4. after this we can manually enter value from result grid or by writing code
+
+#while creating lat log column data trunication error was occured
